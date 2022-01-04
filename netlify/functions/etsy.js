91a1c336a999732a83b7135c0bc8eb369eb6428e
Dustin LaMarr <dustin.lamarr@gmail.com>
2022-01-04 13:38:13 -0700

    Hid api keys in .env file

--- netlify/functions/etsy.js
+++ netlify/functions/etsy.js
@@ -1,21 +1,19 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
+require("dotenv").config();
+const etsyKey = process.env.ETSY_API_KEY;
+
 exports.handler = async () => {
-  // const etsyOptions = {
-  //   method: "GET",
-  //   url: "https://openapi.etsy.com/v3/application/shops/8861893",
-  //   headers: {
-  //     "x-api-key": "uxcmk2zeec8cn1ew5uj7v8s1",
-  //   }
-  // }
-  const etsyRes = await fetch("https://openapi.etsy.com/v3/application/shops/8861893", {
-    headers: {
-          "x-api-key": "uxcmk2zeec8cn1ew5uj7v8s1",
-        }
-  })
-  // const apiData = axios.request(etsyOptions);
-  const etsyData = await etsyRes.json()
+  const etsyRes = await fetch(
+    "https://openapi.etsy.com/v3/application/shops/8861893",
+    {
+      headers: {
+        "x-api-key": `${etsyKey}`,
+      },
+    }
+  );
+  const etsyData = await etsyRes.json();
   return {
-statusCode: 200,
-body: JSON.stringify(etsyData)
-  }
+    statusCode: 200,
+    body: JSON.stringify(etsyData),
+  };
 };
