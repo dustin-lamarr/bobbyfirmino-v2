e716bbac643180817bc60cd7b247f3fdfe567812
Dustin LaMarr <dustin.lamarr@gmail.com>
2022-01-18 16:30:02 -0700

    Changed table api to fetch

--- netlify/functions/plTableAPI.js
+++ netlify/functions/plTableAPI.js
@@ -1,23 +1,21 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
-const plKey = process.env.PL_API_KEY
+const plKey = process.env.PL_API_KEY;
 
 exports.handler = async () => {
   const plTable = await fetch(
-    "https://viperscore.p.rapidapi.com/competition/standings",
-    {
-        params: { competitionId: "17", seasonId: "37036" },
-      headers: {
+    "https://viperscore.p.rapidapi.com/competition/standings?competitionId=9464&seasonId=27719",
+    {"method": "GET",
+      "headers": {
         "x-rapidapi-host": "viperscore.p.rapidapi.com",
         "x-api-key": `${plKey}`,
-      },
-    }
+      }
+  }
   );
   const plTableData = await plTable.json();
-  console.log(plTableData)
+  console.log(plTableData);
   return {
     statusCode: 200,
     body: JSON.stringify(plTableData),
   };
-
 };
