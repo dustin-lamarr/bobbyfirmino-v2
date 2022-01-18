e3fca429a564c4d442a7f46947c826a4e3896032
Dustin LaMarr <dustin.lamarr@gmail.com>
2022-01-18 16:57:56 -0700

    Had to update pl table api because of changes to endpoint

--- netlify/functions/plTableAPI.js
+++ netlify/functions/plTableAPI.js
@@ -4,14 +4,16 @@ const plKey = process.env.PL_API_KEY;
 
 exports.handler = async () => {
   const plTable = await fetch(
-    "https://viperscore.p.rapidapi.com/competition/standings?competitionId=9464&seasonId=27719",
-    {"method": "GET",
-      "headers": {
+    "https://viperscore.p.rapidapi.com/competition/standings?competitionId=17&seasonId=37036",
+    {method: "GET",
+      headers: {
         "x-rapidapi-host": "viperscore.p.rapidapi.com",
         "x-api-key": `${plKey}`,
       }
   }
-  );
+  ).catch(err => {
+    console.error(err);
+  });
   const plTableData = await plTable.json();
   console.log(plTableData);
   return {
